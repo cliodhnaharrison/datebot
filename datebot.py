81ba327a147ed61f3ae8283dc039268e31082782
Cliodhna Harrison <cliodhnaharrison@gmail.com>
2021-08-22 01:32:49 +0100

    Initial datebot

---
+++ datebot.py
@@ -0,0 +1,64 @@
+import os.path
+
+class Date:
+
+    def __init__(self, name, pricepoint, dress_code, effort, location):
+        self.name = name
+        self.pricepoint = pricepoint
+        self.dress_code = dress_code
+        self.effort = effort
+        self.location = location
+
+    def __str__(self):
+        return self.name
+
+    def get_name(self):
+        return self.name
+
+    def get_pricepoint(self):
+        return self.pricepoint
+
+    def get_dress_code(self):
+        return self.dress_code
+
+    def get_effort(self):
+        return self.effort
+
+    def get_location(self):
+        return self.location
+
+
+def load_dates():
+    reader = open("dates.csv", "r")
+    dates = []
+    for line in reader.readlines():
+        d = Date(*[s.lower() for s in line.strip().split(",")])
+        dates.append(d)
+    return dates
+
+
+def decide_date(pricepoint, dress_code, effort, location):
+    dates = load_dates()
+    results = []
+    for date in dates:
+        if (date.get_effort() == effort and
+        date.get_pricepoint() == pricepoint and
+        date.get_dress_code() == dress_code and
+        date.get_location() == location):
+            results.append(date)
+
+    return results
+
+def main():
+    os.path.isfile("/dates.csv")
+    pricepoint = input("What is your pricepoint? ").lower()
+    dress_code = input("What is your dress code? ").lower()
+    effort = input("What is your effort? ").lower()
+    location = input("What is your location? ").lower()
+    results = decide_date(pricepoint, dress_code, effort, location)
+
+    for r in results:
+        print (r)
+
+if __name__ == "__main__":
+    main()
